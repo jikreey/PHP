2bd8284ea0f8a71d91d9f2486cfb837bdd59d69f
jikreey <dzikri.ugm@gmail.com>
2016-03-28 15:07:52 +0700

    Create 10-Switch.php

---
+++ learning/10-Switch.php
@@ -0,0 +1,17 @@
+<?php
+  $today = "monday";
+  
+  switch($today){
+  case "monday":
+    echo "This is monday";
+    break;
+  case "tuesday":
+    echo "This is tuesday";
+    break;
+  case "wednesday":
+    echo "This is wednesday";
+    break;
+  default:
+    echo "the hell???";
+  }
+?>
