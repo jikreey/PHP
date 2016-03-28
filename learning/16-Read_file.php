c4de09823862626f392deecaf15d4534f196ccc0
jikreey <dzikri.ugm@gmail.com>
2016-03-28 15:22:33 +0700

    Create 16-Read_file.php

---
+++ learning/16-Read_file.php
@@ -0,0 +1,12 @@
+<?php
+  $read = file('names.txt');
+  $count = count($read);
+  $i = 1;
+  foreach ($read as $line) {
+    echo $line;
+    if($i < $count) {
+      echo ', ';
+    }
+    $i++;
+  }
+?>
