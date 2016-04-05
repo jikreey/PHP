1d7e32f7d29f40e37c8d4a55a111483bcd54bba0
jikreey <dzikri.ugm@gmail.com>
2016-04-05 18:11:03 +0700

    Create 05-Insert_data_with_lastid(SQLi).php

---
+++ 05-Insert_data_with_lastid(SQLi).php
@@ -0,0 +1,24 @@
+<?php
+//buat koneksi dengan MySQL
+$connect=mysqli_connect('localhost','root','','myDB');
+ 
+//jika koneksi gagal, langsung keluar dari PHP
+if (!$connect)
+{
+   die("Koneksi dengan MySQL gagal");
+}
+
+//membuat table
+$data = "INSERT INTO visitor (firstname, lastname, email)
+		VALUES ('syafiq', 'hibatullah', 'syafiq.hibatullah@gmail.com')";
+
+
+if(mysqli_query($connect,$data)) {
+	$last_id = mysqli_insert_id($connect);
+	echo "Data created successfully. Last ID inserted: " . $last_id;
+} else {
+	echo "Data unsuccessfully created: " . mysqli_error($connect); 
+}
+
+mysqli_close($connect);
+?>
