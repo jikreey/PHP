e6128e30704c311a6da6baa1babc87f86e6e92df
jikreey <dzikri.ugm@gmail.com>
2016-04-05 17:30:51 +0700

    Create 04-Insert_data(SQLi).php

---
+++ 04-Insert_data(SQLi).php
@@ -0,0 +1,23 @@
+<?php
+//buat koneksi dengan MySQL
+$connect=mysqli_connect('localhost','root','','myDB');
+ 
+//jika koneksi gagal, langsung keluar dari PHP
+if (!$connect)
+{
+   die("Koneksi dengan MySQL gagal");
+}
+
+//membuat table
+$data = "INSERT INTO visitor (firstname, lastname, email)
+		VALUES ('dzikri', 'purnama', 'admin@dzikri.id')";
+
+
+if(mysqli_query($connect,$data)) {
+	echo "Data created successfully";
+} else {
+	echo "Data unsuccessfully created: " . mysqli_error($connect); 
+}
+
+mysqli_close($connect);
+?>
