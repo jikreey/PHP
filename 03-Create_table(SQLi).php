db446af8348c1158ea83058f04f81202501b60e4
jikreey <dzikri.ugm@gmail.com>
2016-04-05 17:22:52 +0700

    Create 03-Create_table(SQLi).php

---
+++ 03-Create_table(SQLi).php
@@ -0,0 +1,25 @@
+<?php
+//buat koneksi dengan MySQL
+$connect=mysqli_connect('localhost','root','','myDB');
+ 
+//jika koneksi gagal, langsung keluar dari PHP
+if (!$connect)
+{
+   die("Koneksi dengan MySQL gagal");
+}
+
+//membuat table
+$table = "CREATE TABLE visitor(
+		id INT(6) UNSIGNED PRIMARY KEY,
+		firstname VARCHAR(30) NOT NULL,
+		lastname VARCHAR(30) NOT NULL,
+		email VARCHAR(30)
+		)";
+
+
+if(mysqli_query($connect,$table)) {
+	echo "Table created successfully";
+} else {
+	echo "Table unsuccessfully created" . mysqli_error($connect); 
+}
+?>
