e02e00c5a03a26de505897ed80deb17311255d50
jikreey <dzikri.ugm@gmail.com>
2016-04-05 22:25:52 +0700

    Create index.php

---
+++ inputForm/index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Index visitor</title>
+</head>
+<body>
+	<form name="input" method="post" action="input.php">
+	<table>
+		<tr>
+			<th colspan="2">Input visitor</th>
+		</tr>
+		<tr>
+			<td>nama depan
+			: <input type="text" name="firstname"></td>
+		</tr>
+		<tr>
+			<td>nama belakang
+			: <input type="text" name="lastname"></td>
+		</tr>
+		<tr>
+			<td>email
+			: <input type="email" name="email"></td>
+		</tr>
+		<tr>
+			<td><input type="submit" name="submit"></td>
+		</tr>		
+	</table>
+</body>
+</html>
